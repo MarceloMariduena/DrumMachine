7b0493f9e64b21fb32315d913a2a62129ec6db18
Marcelo Mariduena <MarceloMariduena@protonmail.com>
2019-02-15 13:56:29 -0500

    Updates

---
+++ precache-manifest.69f43c90690d864e353c48d83c2be1c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3c605de54f607651da",
+    "url": "/MarceloMariduena/DrumMachine/static/css/main.f70fd6ca.chunk.css"
+  },
+  {
+    "revision": "6e3c605de54f607651da",
+    "url": "/MarceloMariduena/DrumMachine/static/js/main.6e3c605d.chunk.js"
+  },
+  {
+    "revision": "f0ce3810e6cbf4a5dc02",
+    "url": "/MarceloMariduena/DrumMachine/static/js/runtime~main.f0ce3810.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/MarceloMariduena/DrumMachine/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MarceloMariduena/DrumMachine/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e6e52a83f2f04ebd311379f5eb5c682",
+    "url": "/MarceloMariduena/DrumMachine/index.html"
+  }
+];
